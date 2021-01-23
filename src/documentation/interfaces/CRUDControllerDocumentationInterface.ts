232a0d360e6609c0d47a0532315d74770fb1a03a
Florian Varrin <florian.varrin@gmail.com>
2021-01-23 17:35:09 +0100

    Fix .gitignore file

---
+++ src/documentation/interfaces/CRUDControllerDocumentationInterface.ts
@@ -0,0 +1,9 @@
+interface CRUDControllerDocumentationInterface {
+  controller: {
+    create: Record<string, unknown>;
+    findAll: Record<string, unknown>;
+    findOne: Record<string, unknown>;
+    update: Record<string, unknown>;
+    remove: Record<string, unknown>;
+  };
+}
---
+++ src/documentation/interfaces/CRUDDTODocumentationInterface.ts
@@ -0,0 +1,5 @@
+interface CRUDDTODocumentationInterface {
+  dto: {
+    create: Record<string, unknown>;
+  };
+}
